dc4fe2eadcc706e55844e8f450e5b9f20d5d0ea8
Daniel JCH <danielj.callisayah@gmail.com>
2021-12-21 12:17:16 -0400

    button animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.brown
+                .ignoresSafeArea()
+            Button {
+                animationAmount += 0.5
+            } label:{
+                Text("Press Me")
+                    .padding(40)
+                   .contentShape(Circle())
+            }
+               
+            .background(Color(.sRGB, red: 0.2 + animationAmount / 3, green: 0.3 * animationAmount, blue: 1-animationAmount/2, opacity: 1))
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+           // .blur(radius: (animationAmount - 1) * 3)
+         //   .animation(.interpolatingSpring(stiffness: 40, damping: 0.1), value: animationAmount)
+            .animation(.easeInOut(duration: 0.5), value: animationAmount)
+            
+        }
     }
 }
 
