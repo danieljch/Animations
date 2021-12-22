93340dc5660b61a2b52615f44f6dc9ba026d839c
Daniel JCH <danielj.callisayah@gmail.com>
2021-12-22 12:39:08 -0400

    Views with transition

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,28 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    @State private var isShowingRed = false
     
     var body: some View {
-        ZStack {
-            Color.brown
-                .ignoresSafeArea()
-            Button {
-                animationAmount += 0.5
-            } label:{
-                Text("Press Me")
-                    .padding(40)
-                   .contentShape(Circle())
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
             }
-               
-            .background(Color(.sRGB, red: 0.2 + animationAmount / 3, green: 0.3 * animationAmount, blue: 1-animationAmount/2, opacity: 1))
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount)
-           // .blur(radius: (animationAmount - 1) * 3)
-         //   .animation(.interpolatingSpring(stiffness: 40, damping: 0.1), value: animationAmount)
-            .animation(.easeInOut(duration: 0.5), value: animationAmount)
-            
+
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
         }
     }
 }
